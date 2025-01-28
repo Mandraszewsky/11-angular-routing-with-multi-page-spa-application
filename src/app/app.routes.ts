8ea0c829b5635a8ef3392a0e86331312cd32918e
Mandraszewsky <mandrah@o2.pl>
2025-01-28 15:41:50 +0100

    nested (children) routes with links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
@@ -15,6 +16,16 @@ export const routes: Routes = [
 
         // dynamic route, "/:" syntax tells angular that is dynamic segment:
         path: 'users/:userId',
-        component: UserTasksComponent
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', //users/userId/tasks
+                component: TasksComponent,
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent,
+            }
+        ]
     }
 ]
