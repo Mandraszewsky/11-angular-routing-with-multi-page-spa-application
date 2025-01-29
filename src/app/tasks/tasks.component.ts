a420a4b2397d11bb0dd58e2a2bf9196b696acb8e
Mandraszewsky <mandrah@o2.pl>
2025-01-29 10:54:19 +0100

    query parameters (extracting & using)

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,22 +2,32 @@ import { Component, input, inject, computed } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   private tasksService = inject(TasksService);
 
   // extract dynamic route parameter (path: 'users/:userId') via input binding, accessable via paramsInheritanceStrategy config
   userId = input.required<string>();
+  // query parameters:
+  order = input<'asc' | 'desc'>('desc'); // this will be set automatically thanks by withComponentInputBinding() configuration, delivered in app.config.ts
 
-  userTasks = computed(() => 
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+  userTasks = computed(() =>
+    this.tasksService.allTasks().filter((task) => task.userId === this.userId()).sort((a, b) => {
+      if (this.order() === 'desc') {
+        return a.id > b.id ? -1 : 1;
+      }
+      else {
+        return a.id > b.id ? 1 : -1;
+      }
+    })
   );
 
 }
