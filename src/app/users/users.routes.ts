024eeded5808e142580c388d45f984e910ab8437
Mandraszewsky <mandrah@o2.pl>
2025-01-29 13:41:32 +0100

    lazy loading service example

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,19 +1,26 @@
 import { Routes } from "@angular/router";
 import { TasksComponent } from "../tasks/tasks.component";
 import { NewTaskComponent } from "../tasks/new-task/new-task.component";
+import { TasksService } from "../tasks/tasks.service";
 
 export const routes: Routes = [
     {
-        path: '', // redirection to /tasks, the difference between prefix and full parameter is how they checks url
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
+        path: '',
+        providers: [TasksService],
+        children: [
+            {
+                path: '', // redirection to /tasks, the difference between prefix and full parameter is how they checks url
+                redirectTo: 'tasks',
+                pathMatch: 'prefix',
+            },
+            {
+                path: 'tasks', //users/userId/tasks
+                component: TasksComponent,
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent,
+            }
+        ],
     },
-    {
-        path: 'tasks', //users/userId/tasks
-        component: TasksComponent,
-    },
-    {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-    }
 ]
