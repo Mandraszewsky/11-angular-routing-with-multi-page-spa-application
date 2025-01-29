f6e9ccba34d659ca909b9fb95795c1669da71f30
Mandraszewsky <mandrah@o2.pl>
2025-01-29 12:14:34 +0100

    programmatic navigation (via angular router & guard)

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -4,6 +4,7 @@ import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
+// import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-task',
@@ -16,7 +17,19 @@ export class TaskComponent {
   task = input.required<Task>();
   private tasksService = inject(TasksService);
 
+  // programmatic navigation example:
+  // private router = inject(Router);
+  // private activatedRoute = inject(ActivatedRoute);
+
   onComplete() {
     this.tasksService.removeTask(this.task().id);
+
+    // programmatic navigation example:
+    // this.router.navigate(['./'], {
+    //   relativeTo: this.activatedRoute,
+    //   onSameUrlNavigation: 'reload',
+    //   queryParamasHandling: 'preserve',
+    // });
+    // also in app.routes.ts for this component, runGuardsAndResolvers needs to be set up as 'always'
   }
 }
